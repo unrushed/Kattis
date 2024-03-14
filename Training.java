3e06a5d2c7e8a45c2e169c63808665c4228ccdaf
Ryan Hayden <ryanhpersonalemail@gmail.com>
2024-03-14 13:38:10 -0400

    Finished with a time of 680ms Add Training.java

---
+++ Training.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Training {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numberOfProblems = input.nextInt();
+        int skillLevel = input.nextInt();
+
+        for (int i = 0; i < numberOfProblems; i++) {
+            int tempLow = input.nextInt();
+            int tempHigh = input.nextInt();
+            if(skillLevel >= tempLow && skillLevel <= tempHigh){
+                skillLevel++;
+            }
+        }
+        System.out.println(skillLevel);
+    }
+}
