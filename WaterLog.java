0a14543b425dba40c0c223c2aeb3ff161b02a81b
Ryan Hayden <ryanhpersonalemail@gmail.com>
2024-06-01 20:39:23 -0400

    6/1/24 Adding WaterLog.java

---
+++ WaterLog.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class WaterLog {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numberOfDays = input.nextInt(), minCupsOfWater = input.nextInt(), maxCupsOfWater = input.nextInt();
+        boolean test = false;
+        boolean test2 = false;
+        for (int i = 0; i < numberOfDays - 1; i++) {
+            int temp = input.nextInt();
+            if(temp == minCupsOfWater) test = true;
+            if(temp == maxCupsOfWater) test2 = true;
+        }
+
+        if((!(test)) && (!(test2))){
+            System.out.println("-1");
+        } else if (!(test)) {
+            System.out.println(minCupsOfWater);
+        } else if (!(test2)) {
+            System.out.println(maxCupsOfWater);
+        }else{
+            printAll(minCupsOfWater, maxCupsOfWater);
+        }
+
+
+    }
+    public static void printAll(int a, int b){
+        while(!(a > b)){
+            System.out.println(a++);
+        }
+    }
+
+}
