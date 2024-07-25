2ce7f96b67cefd5800114356765659d8703dd7b8
Ryan Hayden <ryanhpersonalemail@gmail.com>
2024-07-24 20:54:27 -0400

    7/24/24 Adding 2 Programs

---
+++ Rectangle2.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Rectangle2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] z = new int[3];
+
+        int[] f2 = new int[3];
+        for (int f = 0, i = 0; f < 3; f++, i++) {
+            z[f] = input.nextInt();
+            f2[i] = input.nextInt();
+        }
+
+        Arrays.sort(z);
+        Arrays.sort(f2);
+
+        if(z[0] == z[1]) System.out.print(z[2]);
+        else System.out.print(z[0]);
+
+        System.out.print(" ");
+
+        if(f2[0] == f2[1]) System.out.print(f2[2]);
+        else System.out.print(f2[0]);
+    }
+}
---
+++ pizzaA.java
@@ -0,0 +1,5 @@
+public class pizzaA {
+    public static void main(String[] args) {
+
+    }
+}
