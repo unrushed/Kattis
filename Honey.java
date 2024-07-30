523f28d85921aade41921eb2ad509bce6b772cde
Ryan Hayden <ryanhpersonalemail@gmail.com>
2024-07-29 20:44:28 -0400

    7/24/24 Adding Honey.java

---
+++ Honey.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Honey {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while(true){
+            int sweet = input.nextInt();
+            int sour = input.nextInt();
+
+            if(sweet == 0 && sour == 0) break;
+
+            if(sweet + sour == 13) {
+                System.out.println("Never speak again.");
+            }else if( sweet == sour){
+                System.out.println("Undecided.");
+            }else if(sour > sweet){
+                System.out.println("Left beehind.");
+            }else{
+                System.out.println("To the convention.");
+            }
+        }
+    }
+}
