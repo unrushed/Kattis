ba3918fd49014eded2be458344cdd7e4dc72e7c1
Ryan Hayden <ryanhpersonalemail@gmail.com>
2024-09-25 17:53:32 -0400

    9/25/24

---
+++ Done/ameriskur.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ameriskur{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double a = input.nextInt();
+        System.out.print(a*0.09144);
+    }
+}
